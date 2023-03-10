ba066bdb550df0656a2a4401b0ab1c86e49d1506
unknown <erdematik8@gmail.com>
2023-03-10 23:39:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import parseLatLong from './parseLatLong';
+
+
+function CoordinateForm(props) {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const parsedCoordinate = parseLatLong(input);
+    console.log(parsedCoordinate);
+    props.addCoordinate(parsedCoordinate);
+    setInput('');
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter coordinates:
+        <input type="text" value={input} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+
+function CoordinateList(props) {
+  return (
+    <ul>
+      {props.coordinates.map((coord, index) => (
+        <li key={index}>
+          Latitude: {coord[0]}, Longitude: {coord[1]}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
 
 function App() {
+  const [coordinates, setCoordinates] = useState([]);
+
+  const addCoordinate = (coordinate) => {
+    setCoordinates([...coordinates, coordinate]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Coordinate List</h1>
+      <CoordinateForm addCoordinate={addCoordinate} />
+      <CoordinateList coordinates={coordinates} />
     </div>
   );
 }
+
 
 export default App;
